9f6c5cd5a9ce017ba013e6b0d4b1a14aa2818184
fritzkuechli <fritzchensrevenge@gmail.com>
2021-11-23 07:43:50 +0100

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        Scanner inp = new Scanner(System.in);
+        boolean question = true;
+        int maxQ = 0;
+
+        do {
+            try {
+                System.out.println("input your number! ");
+                numbers.add(inp.nextInt());
+            } catch (Exception e) {
+                System.out.println("you wanna q?");
+                if (inp.next().equals("q")) {
+                    question = false;
+                } else {
+                   continue;
+                }
+            }
+        } while (question);
+
+        Collections.sort(numbers);
+
+        for (int n : numbers) {
+            System.out.format(" %d ", n);
+        }
+        System.out.println("The maximum element is: " +  numbers.get(numbers.size() -1 ));
     }
 }
